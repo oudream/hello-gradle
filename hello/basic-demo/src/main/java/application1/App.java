6ea6615b1799143a57b32555bebaca23fedae1dc
oudream <oudream@126.com>
2019-12-16 09:50:38 +0800

    up

--- hello/basic-demo/src/main/java/application1/App.java
+++ hello/basic-demo/src/main/java/application1/App.java
@@ -8,7 +8,12 @@ public class App {
         return "Hello world.";
     }
 
+    public static String name = "xx";
+
     public static void main(String[] args) {
+        int i1 = 2;
+        String s1 = "aaabbb123";
+        System.out.println(s1);
         System.out.println(new App().getGreeting());
     }
 }
