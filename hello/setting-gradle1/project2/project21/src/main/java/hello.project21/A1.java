ea87afb3bb62be9ea47a27d8c92f99f63adc6c51
oudream <oudream@126.com>
2020-02-21 12:11:03 +0800

    multi project

---
+++ hello/setting-gradle1/project2/project21/src/main/java/hello.project21/A1.java
@@ -0,0 +1,11 @@
+package hello.project21;
+
+// import java.io;
+
+public class A1 {
+        public static void main1(String argv[]) {
+//             System.out.println(Document2.showName());
+//             System.out.printLn("aa");
+        }
+
+}
---
+++ hello/setting-gradle1/project2/project21/src/main/java/hello.project21/A2.java
@@ -0,0 +1,7 @@
+package hello.project21;
+
+public class A2 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
